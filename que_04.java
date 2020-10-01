bacaa4f10284737190d0b13c6a0e9626d60e716c
NIKHILSINGH1510 <56766208+NIKHILSINGH1510@users.noreply.github.com>
2020-10-01 18:51:38 +0530

    Add files via upload

---
+++ que_04.java
@@ -0,0 +1,14 @@
+public class que_04 {
+    public static void main(String[] args)
+    {
+        int p=9900;
+        int t=3;
+        int r=7;
+        float I=(p*r*t)/100;
+        System.out.println("simple interest is equal :"+I);
+
+
+
+    }
+    
+}
